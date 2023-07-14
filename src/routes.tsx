d5da7da2f1cd13c4725e48332b4187b7d15840e9
Antonio Annes <antonio.vmachado13@gmail.com>
2023-07-14 11:19:24 -0300

    perfil

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Perfil from './pages/Perfil'
 // import Categories from './pages/Categories'
 // import Product from './pages/Product'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    {/* <Route path="/categories" element={<Categories />} /> */}
+    <Route path="/perfil" element={<Perfil />} />
     {/* <Route path="/product/:id" element={<Product />} /> */}
   </Routes>
 )
